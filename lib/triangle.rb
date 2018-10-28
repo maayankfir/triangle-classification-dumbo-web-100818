18e80472b57b448857c7cc97f3521a44303247bd
github username <github email address>
2018-10-28 18:31:25 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
+
   # write code here
+  def initialize(x,y,z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if @x < 0 || @y < 0 || @z < 0
+      raise TriangleError
+    elsif @x == 0 || @y == 0 || @z == 0
+      raise TriangleError
+    elsif @x + @y <= @z || @x + @z <= @y || @y + @z <= @x
+      raise TriangleError
+    elsif @x == @y && @y == @z
+       :equilateral
+    elsif @x == @y || @x == @z || @y == @z
+       :isosceles
+    elsif @x + @y > @z || @x + @z > @y || @y + @z > @x
+       :scalene
+
+
+    else
+      raise TriangleError
+
+    end
+  end
+
+  class TriangleError < StandardError
+   # triangle error code
+ end
 end
